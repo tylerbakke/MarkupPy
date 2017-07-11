f9fcde00b858ce6a7b6daf332788aa1cd4800bab
Tyler Bakke <tbakke@splunk.com>
2017-07-11 13:38:21 -0700

    Initial setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='markup.py',
+    version='1.10',
+    description='Markup.py is an intuitive, light weight, easy-to-use, customizable and pythonic HTML/XML generator',
+    url='http://github.com/tylerbakke/markup.py',
+    author='Daniel Nogradi',
+    author_email='nogradi@gmail.com',
+    license='MIT',
+    packages=['markup.py'],
+    zip_safe=False)
