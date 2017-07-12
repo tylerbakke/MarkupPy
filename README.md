9af6454566d59a6e81bad8c4f58bb828431b07b6
Tyler Bakke <tbakke@splunk.com>
2017-07-11 23:48:45 -0700

    Finalized Python Package

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# markup.py
+An HTML/XML generator
+
+This is markup.py - a Python module that attempts to make it easier to generate HTML/XML from a Python program in an intuitive, lightweight, customizable and pythonic way. It works with both python 2 and 3.
+
+The code is in the public domain.
+
+Version: 1.10 as of 16 March 2015.
+
+Documentation and further info is at http://markup.sourceforge.net/
+
+Please send bug reports, feature requests, enhancement ideas or questions to nogradi at gmail dot com.
+
+Installation: Run "pip install markup.py" from the terminal.
--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import setup
+import textwrap
 
 setup(name='markup.py',
-    version='1.10',
-    description='Markup.py is an intuitive, light weight, easy-to-use, customizable and pythonic HTML/XML generator',
-    url='http://github.com/tylerbakke/markup.py',
-    author='Daniel Nogradi',
-    author_email='nogradi@gmail.com',
-    license='MIT',
-    packages=['markup.py'],
-    zip_safe=False)
+      version='1.11',
+      description='Markup.py is an intuitive, light weight, easy-to-use, customizable and pythonic HTML/XML generator',
+      url='http://markup.sf.net/',
+      author='Daniel Nogradi',
+      author_email="nogradi@gmail.com",
+      license="MIT",
+      packages=['markup'],
+      classifiers=[
+          'Environment :: Console',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3'
+      ],
+      zip_safe=False)
